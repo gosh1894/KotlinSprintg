a877529cd67bea757d8f7e9572747cb11a7f5d4c
gosh1894 <gurgen94@outlook.com>
2026-02-14 08:42:36 +0300

    KS-1-5 refactor task5

--- app/src/main/kotlin/lesson_1/lesson1_task5.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,15 +1,16 @@
 package lesson_1
 
+const val SECONDS_IN_HOUR = 3600
+const val SECONDS_IN_MINUTE = 60
+
 fun main() {
-
     val totalSeconds = 6480
-    val hours = totalSeconds / 3600
-    val secondsAfterHours = totalSeconds % 3600
-    val minutes = secondsAfterHours / 60
-    val seconds = secondsAfterHours % 60
-    val h = if (hours < 10) "0$hours" else "$hours"
-    val m = if (minutes < 10) "0$minutes" else "$minutes"
-    val s = if (seconds < 10) "0$seconds" else "$seconds"
 
-    println("$h:$m:$s")
+    val hours = totalSeconds / SECONDS_IN_HOUR
+    val minutes = (totalSeconds % SECONDS_IN_HOUR) / SECONDS_IN_MINUTE
+    val seconds = totalSeconds % SECONDS_IN_MINUTE
+
+    val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+
+    println(formattedTime)
 }
