c9d3487513b5ac89297eeb3ff355de03d6b0f598
gosh1894 <gurgen94@outlook.com>
2026-02-14 09:01:43 +0300

    KS-3-1 task1

---
+++ app/src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,12 @@
+package lesson_3
+
+fun main() {
+    val userName =  "Гоша"
+
+    var greting: String = "Добрый день!, $userName"
+    println(greting)
+
+    greting = "Добрый вечер!, $userName"
+    println(greting)
+
+}
