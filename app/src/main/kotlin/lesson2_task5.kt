aade4c3ff86607689fe988b15f154b64e10a5fbb
gosh1894 <gurgen94@outlook.com>
2026-02-15 08:54:54 +0300

    KS-2-5 refactor task5

--- app/src/main/kotlin/lesson2_task5.kt
+++ app/src/main/kotlin/lesson2_task5.kt
@@ -2,14 +2,14 @@ import kotlin.math.pow
 
 fun main() {
 
-        val initialAmount = 70_000.0
-        val annualRate = 0.167
-        val years = 20
+    val initialAmount = 70_000.0
+    val annualRatePercent = 16.7
+    val years = 20
+    val rateModifier = annualRatePercent / 100
+    val finalAmount = initialAmount * (1 + rateModifier).pow(years)
+    val formattedResult = "%.3f".format(finalAmount)
 
-        val finalAmount = initialAmount * (1 + annualRate).pow(years)
-        val formattedResult = "%.3f".format(finalAmount)
-
-        println("Сумма вклада через $years лет составит: $formattedResult")
-    }
+    println("Сумма вклада через $years лет составит: $formattedResult")
+}
 
 
