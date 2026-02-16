6d0dbdfd2936c9bb4a781797fd17f0195956bca8
gosh1894 <gurgen94@outlook.com>
2026-02-16 10:10:26 +0300

    KS-3-4 task4

---
+++ app/src/main/kotlin/lessson_3/lesson3_task4.kt
@@ -0,0 +1,19 @@
+package lessson_3
+
+fun main() {
+
+    var from = "E2"
+    var to = "E4"
+    var moveNumber = 1
+    var message = "($from-$to;$moveNumber)"
+
+    println("Ход 1: $message")
+
+    from = "D2"
+    to = "D3"
+    moveNumber = 2
+    message = "($from-$to;$moveNumber)"
+
+    println("Ход 2: $message")
+
+}
