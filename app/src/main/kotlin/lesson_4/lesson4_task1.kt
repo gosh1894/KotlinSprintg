90ea7ff9f797f109bc08e179236edf635733c50a
gosh1894 <gurgen94@outlook.com>
2026-02-17 09:47:44 +0300

    KS-4-1 task1

---
+++ app/src/main/kotlin/lesson_4/lesson4_task1.kt
@@ -0,0 +1,11 @@
+package lesson_4
+
+fun main() {
+
+    val table = 13
+    val reservedTable = 13
+    val tomorrowReservedTable = 9
+    println("Общие столики,$table,Сегодня забронированно,$reservedTable,Завтра,$tomorrowReservedTable")
+
+
+}
