bbe3743bb062585e78db226285b407edcd888080
gosh1894 <gurgen94@outlook.com>
2026-02-17 10:22:05 +0300

    KS-2-1 refactor task1

--- app/src/main/kotlin/lesson_2/lesson2_task1.kt
+++ app/src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -6,8 +6,9 @@ fun main() {
     val grade2 = 4
     val grade3 = 3
     val grade4 = 5
+    val grade5 = 4.0
 
-    val average = (grade1 + grade2 + grade3 + grade4) / 4.0
+    val average = (grade1 + grade2 + grade3 + grade4 + grade5)
 
     println(String.format("%.2f", average))
 }
