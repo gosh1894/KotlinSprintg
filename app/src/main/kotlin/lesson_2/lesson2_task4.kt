2b5c583e76797f8c6c67e84c22773da3457762fb
gosh1894 <gurgen94@outlook.com>
2026-02-17 10:29:21 +0300

    KS-2-4 refactor task4

--- app/src/main/kotlin/lesson_2/lesson2_task4.kt
+++ app/src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -3,11 +3,12 @@ package lesson_2
 fun main() {
     val crystalOre = 7
     val ironOre = 11
-    val buffModifier = 20
+    val buffModifier = 0.2
 
     val bonusCrystal = (crystalOre * buffModifier).toInt():
     val bonusIron = (ironOre * buffModifier).toInt()
 
-    println("Бонусная кристаллическая руда: $bonusCrystal")
-    println("Бонусная железная руда: $bonusIron")
+    println("Бонусная кристаллическая руда (+20%): $bonusCrystal")
+    println("Бонусная железная руда (+20%): $bonusIron")
+    println("Итого кристаллов: ${crystalOre + bonusCrystal}")
 }
